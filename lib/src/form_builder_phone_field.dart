1e98142b592ccdd57d61eac5d622907890b2e20e
Wilco Vogt <wilco.vogtkega.nl@MacBook-Pro-van-Wilco.local>
2026-06-27 14:45:45 +0200

    Revert "AI country code fix"
    This reverts commit bd4a885bb96865258b83015036baca62bc9aea52.

--- lib/src/form_builder_phone_field.dart
+++ lib/src/form_builder_phone_field.dart
@@ -399,7 +399,7 @@ class _FormBuilderPhoneFieldState
           onValuePicked: (Country country) {
             effectiveFocusNode.requestFocus();
             setState(() => _selectedDialogCountry = country);
-            super.didChange(fullNumber);
+            didChange(fullNumber);
           },
           itemFilter: widget.countryFilterByIsoCode != null
               ? (c) => widget.countryFilterByIsoCode!.contains(c.isoCode)
@@ -457,7 +457,7 @@ class _FormBuilderPhoneFieldState
           isSearchable: widget.isSearchable ?? true,
           onCountrySelected: (Country country) {
             setState(() => _selectedDialogCountry = country);
-            super.didChange(fullNumber);
+            didChange(fullNumber);
             Navigator.of(context).pop();
           },
           countryFilter: widget.countryFilterByIsoCode != null
